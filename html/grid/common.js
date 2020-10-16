7e8ffb1e6bcf4f9ee54f27b48d3e178a316fb41a
공아름 <gal8965@gmail.com>
2020-10-16 17:10:46 +0900

    typo

--- html/grid/common.js
+++ html/grid/common.js
@@ -38,18 +38,21 @@ function init() {
 		function scrollAnim(percent) {
 			var cMax = 7;
 			var cMin = 1;
-			var containerScale = Math.max(cMin,cMax-((cMax-cMin)*(1-percent)));
+			var containerScale = Math.max(cMin,cMax-((cMax-cMin)*(percent)));
 			gsap.from('.screen-container', {scale: containerScale}, {ease: "power1.in"});
+			// $('.screen-container').css({'transform':'scale('+containerScale+')'});
 
 			$('.item').each(function(i){
 				var iMaxX = afterPosArr[i].x;
 				var iMaxY = afterPosArr[i].y;
 				var iMaxScale = afterPosArr[i].scale;
-				var thisPos = { x:iMaxX-(iMaxX*(1-percent)), 
-								y:iMaxY-(iMaxY*(1-percent)), 
-								scale:Math.max(1,iMaxScale-((iMaxScale)*(1-percent)))};
+				var thisPos = { x:iMaxX-(iMaxX*(percent)), 
+								y:iMaxY-(iMaxY*(percent)), 
+								scale:Math.max(1,iMaxScale-((iMaxScale)*(percent)))};
 
 				gsap.from(this, thisPos, {ease: "power1.in"});
+				// console.log($(this), thisPos);
+				// $(this).css({'transform':'translate('+thisPos.x+' '+thisPos.y+',0) scale('+thisPos.scale+')'});
 			});
 		}
 
@@ -57,9 +60,46 @@ function init() {
 			var _top = $(window).scrollTop() - $parent.offset().top;
 			var _move = Math.max(0, $parent.height() - $(window).height());
 			var _percent = Math.max(0, Math.min(1, _top/_move));
-			console.log(_percent);
-
 			scrollAnim(_percent);
+		}
+
+		return {
+			init : _init,
+			scroll : _scroll
+		}
+	}();
+
+	var setPrivacy = function() {
+		var $parent = $('.privacy-wrapper');
+		var $fakeTypo = $parent.find('.typo.fake');
+		var typoText = $parent.find('.typo.real').text();
+		var tLength = typoText.length;
+		var lengPer;
+
+		function _init() {
+			$fakeTypo.html(typoText);
+		}
+
+		function _scroll() {
+			var _top = $(window).scrollTop() - $parent.offset().top;
+			var _move = Math.max(0, $parent.height() - $(window).height());
+			var _percent = Math.max(0, Math.min(1, _top/_move));
+			scrollAnim(_percent);
+		}
+
+		function scrollAnim(percent){
+			lengPer = Math.round(tLength*percent);
+			var curTypo = replaceText(lengPer);
+			$fakeTypo.html(curTypo);
+		}
+
+		function replaceText(index) {
+			var fakeTypo = typoText.substr(0, lengPer) + typoText.substr((tLength-lengPer)+lengPer, tLength);
+			
+			for(var i=0; i<(tLength-lengPer); i++){
+				fakeTypo = fakeTypo + '*';
+			}
+			return fakeTypo;
 		}
 
 
@@ -70,9 +110,11 @@ function init() {
 	}();
 
 	setGrid.init();
+	setPrivacy.init();
 
 	$(window).on('scroll',function(){
 		setGrid.scroll();
+		setPrivacy.scroll();
 	});
 }
 window.onload = init;
